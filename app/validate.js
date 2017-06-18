849ac559a2b10b63af3816279c8035c7a7893927
Marco Worms <marcogworms@gmail.com>
2017-06-18 19:37:45 -0300

    validate: refactor import order

--- app/validate.js
+++ app/validate.js
@@ -1,21 +1,22 @@
 const Ajv = require('ajv')
+const { map } = require('ramda')
+const makeResponse = require('./response')
 
 const ajv = new Ajv()
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
 
-const { map } = require('ramda')
-const makeResponse = require('./response')
+function addValidation (handler) {
+  return (event, context, callback) => {
+    const response = makeResponse(callback)
+    const validate = ajv.compile(handler.inputSchema)
+    const valid = validate(event.body)
 
+    if (!valid) {
+      return response.badRequest(validate.errors)
+    }
 
-module.exports = map(handler => (event, context, callback) => {
-  const response = makeResponse(callback)
-
-  const validate = ajv.compile(handler.inputSchema)
-  const valid = validate(event.body)
-
-  if (!valid) {
-    return response.badRequest(validate.errors)
+    return handler.handle(event, context, callback)
   }
+}
 
-  return handler.handle(event, context, callback)
-})
+module.exports = map(addValidation)
